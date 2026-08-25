7c3cf52fc76771b48764ab0df6c5280d0bc3c9e5
Grizzmon <152689097+Grizzmon@users.noreply.github.com>
2026-08-25 16:47:14 +0200

    update app page

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,25 @@ import { ReviewsSection } from '@/components/reviews-section'
 import { GuaranteeSection } from '@/components/guarantee-section'
 import { FaqSection } from '@/components/faq-section'
 import { SiteFooter } from '@/components/site-footer'
+import { MetaPlansView } from '@/components/meta-plans-view'
 
 export default function Page() {
   return (
     <>
       <SiteHeader />
+
       <main>
         <Hero />
         <TrustSection />
+
+        <MetaPlansView />
         <PlansSection />
+
         <GuaranteeSection />
         <ReviewsSection />
         <FaqSection />
       </main>
+
       <SiteFooter />
     </>
   )
